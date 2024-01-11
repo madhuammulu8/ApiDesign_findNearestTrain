2a4dae77955dd15fad3bf37e9c2c775bc5379811
Madhu <65707202+madhuammulu8@users.noreply.github.com>
2024-01-11 10:02:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import time
+from functools import wraps
+
 import geopandas as gpd
 from fastapi_limiter.depends import RateLimiter
 from shapely.geometry import Point
 from fiona.drvsupport import supported_drivers
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse
+from starlette import status
+from fastapi import Request
+
 from locationcachefile import location_cache, location_cache_lock, load_location_cache
 from geopy.geocoders import Nominatim
 from fastapi_limiter import FastAPILimiter
@@ -45,14 +51,32 @@ def get_address(latitude, longitude):
     return address
 
 
-# print(find_nearest_point(-75.76, 40.0))
+# Code For Rate Limiter Maximum of 10 requests within 60 seconds
+
+def ratelimiter(maximumcalls: int, time_frame: int):
+    def decorator(func):
+        call = []
+
+        @wraps(func)
+        async def wrapper(request:Request, *args, **kwargs):
+            current_time = time.time()
+            calls_in_timeframe = [current_call for current_call in call if current_call > current_time - time_frame]
+
+            if len(calls_in_timeframe) >= maximumcalls:
+                raise HTTPException(status_code=status.HTTP_429_TOO_MANY_REQUESTS, detail="Rate Limit Exceeded")
+            call.append(current_time)
+            return await func(request, *args, **kwargs)
+
+        return wrapper
+    return decorator
 
 
 app = FastAPI()
 
 
 @app.get("/nearestlocation")
-async def get_nearest_location(latitude: float, longitude: float):
+@ratelimiter(maximumcalls=10, time_frame=60)
+async def get_nearest_location(request:Request,latitude: float, longitude: float):
     try:
         result = await find_nearest_point(latitude, longitude)
         address = get_address(latitude, longitude)
